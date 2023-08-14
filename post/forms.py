f2d7c656d46cd70fd7f4f35bd35c9431ba2d6432
LIKELION-EUNTAEK <ahtp8155@naver.com>
2023-08-15 00:08:33 +0900

    feat 주석정리

--- post/forms.py
+++ post/forms.py
@@ -3,5 +3,5 @@ from .models import Board
 
 class BoardForm(forms.ModelForm):
     class Meta:
-        model = Board
-        fields = ['title', 'content', 'photo']
+        model = Board # 이 폼이 사용할 모델은 Board입니다.
+        fields = ['title', 'content', 'photo']  # 폼에 포함될 필드들을 지정합니다. 'title', 'content', 'photo'가 포함됩니다.
--- post/views.py
+++ post/views.py
@@ -14,8 +14,9 @@ def board_detail(request, board_id):
     board.views += 1 # 조회수 증가
     board.save()
     return render(request, 'community/board_detail.html', {'board': board})
+ 
 
-@login_required #로그인 한 사람만 이용 가능
+@login_required # 로그인 한 사람만 이용 가능
 def add_comment(request, board_id):
     board = get_object_or_404(Board, pk=board_id) # 해당 게시물 가져오기
     if request.method == 'POST':
@@ -41,15 +42,15 @@ def like_comment(request, comment_id):
         comment.likes.add(request.user) # 좋아요 추가
     return redirect('board_detail', board_id=comment.board.id)
 
-@login_required
-def create_board(request): # 이미지 업로드 
+@login_required  # 로그인한 사용자만 접근할 수 있는 뷰입니다.
+def create_board(request):
     if request.method == 'POST':
-        form = BoardForm(request.POST, request.FILES)
-        if form.is_valid():
-            board = form.save(commit=False)
-            board.author = request.user
-            board.save()
-            return redirect('board_list')
+        form = BoardForm(request.POST, request.FILES)  # BoardForm 인스턴스를 생성하고 요청 데이터와 파일을 전달합니다.
+        if form.is_valid():  # 폼의 데이터가 유효한지 검사합니다.
+            board = form.save(commit=False)  # 폼으로부터 받은 데이터를 가지고 새로운 Board 모델 인스턴스를 생성합니다.
+            board.author = request.user  # 생성한 게시물의 작성자를 현재 로그인한 사용자로 설정합니다.
+            board.save()  # 변경된 내용을 저장합니다.
+            return redirect('board_list')  # 게시판 목록 페이지로 이동합니다.
     else:
-        form = BoardForm()
+        form = BoardForm()  # GET 요청인 경우 빈 BoardForm 인스턴스를 생성합니다.
     return render(request, 'community/board_form.html', {'form': form})
